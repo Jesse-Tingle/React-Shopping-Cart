d8d266d34eee7c053f9bca4f7d67fdd79f2a43a4
Jesse-Tingle <jtingle0@gmail.com>
2019-10-16 23:31:56 -0400

    add functionality to ShoppingCartItem.js and fixed error for removeItem button

--- src/App.js
+++ src/App.js
@@ -28,21 +28,23 @@ function App() {
 
 
 	return (
-		<ProductContext.Provider value={{ products, addItem }} >
-			<CartContext.Provider value={{ cart, removeItem }} >
-				<div className="App">
-					<Navigation cart={cart} />
+		<CartContext.Provider value={{ cart, removeItem }} >
+			<ProductContext.Provider value={{ products, addItem }} >
+				
+					<div className="App">
+						<Navigation cart={cart} />
 
-					{/* Routes */}
-					<Route exact path="/" component={Products} />
+						{/* Routes */}
+						<Route exact path="/" component={Products} />
 
-					<Route
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
-					/>
-				</div>
-			</CartContext.Provider>
-		</ProductContext.Provider>
+						<Route
+							path="/cart"
+							render={() => <ShoppingCart cart={cart} />}
+						/>
+					</div>
+				
+			</ProductContext.Provider>
+		</CartContext.Provider>
 	);
 }
 
